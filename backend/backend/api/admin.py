01072b5373e600d9d599166592a5e31bdff3479c
Erick Daniel de Sousa Pereira <erickpereira@ads.fiponline.edu.br>
2026-04-12 14:32:31 -0300

    fix: organizando

--- backend/backend/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Aluno, PEI, EvolucaoDiaria, LaudoMedico
-
-admin.site.register(Aluno)
-admin.site.register(PEI)
-admin.site.register(EvolucaoDiaria)
-admin.site.register(LaudoMedico)
